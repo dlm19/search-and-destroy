93454ecdd5c2396152817275302532d31b5dd6e9
Emma Fox <ejfox824@gmail.com>
2020-02-19 16:30:23 -0500

    Broken binary-serach function

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,35 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let midpoint = Math.floor(array.length/2)
+	console.log(midpoint, array)
+	console.log(array[midpoint])
+	if (array[midpoint] === target) {
+		console.log(array[midpoint] === target)
+		//console.log(target)
+		return (array[midpoint] === target)
+	}
+
+	if (array.length === 2) {
+		console.log('inside')
+		if (array[0] === target || array[1] === target) {
+			return true
+		} else {
+			console.log('inside of else')
+			return false
+		}
+	} else {
+		if (array[midpoint] > target) {
+			// array.splice(midpoint)
+			let newArr = array.slice(0, midpoint)
+			binarySearch(newArr, target)
+		} else {
+			let newMidpoint = midpoint + 1
+			// array.splice(0, newMidpoint)
+			let newArr = array.slice(newMidpoint)
+			binarySearch(newArr, target)
+		}
+	}
 };
 
 /*
